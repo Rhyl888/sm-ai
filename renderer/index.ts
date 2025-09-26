2abccc3c066799612f340f82d5790a23b482ab61
鹿森 <2231752346@qq.com>
2025-09-27 00:38:04 +0800

    refactor: refactor project directory structure and add multi-window support.

--- renderer/index.ts
+++ renderer/index.ts
@@ -28,6 +28,9 @@
 
 import './index.css';
 import { createApp } from 'vue';
+import  i18n  from './i18n';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App)
+  .use(i18n)
+  .mount('#app');
