95b28d549ee250ad5c350358c5db9f4e92a398d4
coco <2231752346@qq.com>
2025-11-04 10:59:08 +0800

    feat: add NativeTooltip and ResizeDivider components

--- renderer/App.vue
+++ renderer/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { NConfigProvider } from 'naive-ui';
-import TitleBar from '@renderer/components/TitleBar.vue';
 import DragRegion from '@renderer/components/DragRegion.vue';
 import NavBar from '@renderer/components/NavBar.vue';
+import ResizeDivider from '@renderer/components/ResizeDivider.vue';
+import TitleBar from '@renderer/components/TitleBar.vue';
+import { NConfigProvider } from 'naive-ui';
 
+const sidebarWidth = ref(320);
 
 onMounted(() => {
   console.log('App mounted');
@@ -11,7 +13,7 @@ onMounted(() => {
 </script>
 <template>
   <n-config-provider class="h-full w-[100vw] flex text-tx-primary">
-    <aside class="sidebar h-full flex flex-shrink-0 flex-col w-[320px]">
+    <aside class="sidebar h-full flex flex-shrink-0 flex-col" :style="{ width: sidebarWidth + 'px' }">
       <div class="flex-auto flex">
         <nav-bar />
         <div class="flex-auto">
@@ -19,6 +21,7 @@ onMounted(() => {
         </div>
       </div>
     </aside>
+    <resize-divider direction="vertical" v-model:size="sidebarWidth" :max-size="800" :min-size="320" />
     <div class="flex-auto">
       <title-bar>
         <drag-region class="w-full" />
