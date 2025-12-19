43a3a5809bd6ac15b95e23d26c616889735d45e4
coco <2231752346@qq.com>
2025-12-19 12:13:08 +0800

    feat: add  conversation details page

--- renderer/index.ts
+++ renderer/index.ts
@@ -1,13 +1,13 @@
-import './styles/index.css';
 import 'vfonts/Lato.css';
+import './styles/index.css';
 
 import { createApp, type Plugin } from 'vue';
 import App from '../renderer/App.vue';
 import errorHandler from './utils/errorHandler';
 
-import TitleBar from './components/TitleBar.vue';
-import DragRegion from './components/DragRegion.vue';
 import { createMemoryHistory, createRouter } from 'vue-router';
+import DragRegion from './components/DragRegion.vue';
+import TitleBar from './components/TitleBar.vue';
 import i18n from './i18n';
 
 const components: Plugin = function (app) {
@@ -20,7 +20,18 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      component: () => import('./views/index.vue')
+      component: () => import('./views/index.vue'),
+      children: [
+        {
+          path: '/',
+          redirect: 'conversation'
+        },
+        {
+          name: 'conversation',
+          path: 'conversation/:id?',
+          component: () => import('./views/conversation.vue')
+        }
+      ]
     }
   ]
 });
--- renderer/views/index.vue
+++ renderer/views/index.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-import type { SelectValue } from '@renderer/types'
-import MessageInput from '@renderer/components/MessageInput.vue';
-import CreateConversation from '@renderer/components/CreateConversation.vue';
+// import type { SelectValue } from '@renderer/types'
+// import MessageInput from '@renderer/components/MessageInput.vue';
+// import CreateConversation from '@renderer/components/CreateConversation.vue';
 
-const message = ref('');
-const provider = ref<SelectValue>();
+// const message = ref('');
+// const provider = ref<SelectValue>();
 
-const providerId = computed(() => ((provider.value as string)?.split(':')[0] ?? ''));
-const selectedModel = computed(() => ((provider.value as string)?.split(':')[1] ?? ''));
+// const providerId = computed(() => ((provider.value as string)?.split(':')[0] ?? ''));
+// const selectedModel = computed(() => ((provider.value as string)?.split(':')[1] ?? ''));
 
-const { t } = useI18n();
+// const { t } = useI18n();
 
-async function handleCreateConversation(create: (title: string) => Promise<number | void>, _message: string) {
-  const id = await create(_message);
-  if (!id) return;
-  //  afterCreate
-}
+// async function handleCreateConversation(create: (title: string) => Promise<number | void>, _message: string) {
+//   const id = await create(_message);
+//   if (!id) return;
+//   //  afterCreate
+// }
 </script>
 <template>
   <div class="main-view h-full w-full flex flex-col">
@@ -23,13 +23,12 @@ async function handleCreateConversation(create: (title: string) => Promise<numbe
       <drag-region class="w-full" />
     </title-bar>
     <main class="flex-auto">
-      <!-- <router-view /> -->
-      <!-- main -->
-      <create-conversation :providerId="providerId" :selectedModel="selectedModel" v-slot="{ create }">
+      <router-view />
+      <!-- <create-conversation :providerId="providerId" :selectedModel="selectedModel" v-slot="{ create }">
 
         <message-input v-model:message="message" v-model:provider="provider"
           :placeholder="t('main.conversation.placeholder')" @send="handleCreateConversation(create, message)" />
-      </create-conversation>
+      </create-conversation> -->
     </main>
   </div>
 </template>
